859e7934a1b81ba9593157f95ca4095874f2ac41
slobodan.adamovic <adamovicslobodan@yahoo.com>
2025-01-06 23:58:05 +0100

    new sections added

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const timeline = gsap.timeline();
-
-// Section 1 animation (letters loading with exotic effect)
 const letters = document.querySelectorAll("#letters span");
-letters.forEach((letter) => {
-  letter.style.opacity = 0;
-  letter.style.display = "block";
-});
+const typingText = document.querySelector("#typing-text");
+const textContent = typingText.textContent;
+typingText.textContent = "";
+
+// Section 1 animation
 timeline
   .to("#section1", {
-    backgroundColor: "#000",
     duration: 0.5,
     ease: "power2.out",
     opacity: 1,
@@ -38,7 +36,6 @@ timeline
     ease: "power2.in",
   })
   .to("#section1", {
-    opacity: 1,
     duration: 0.5,
     ease: "power2.inOut",
   });
@@ -65,18 +62,16 @@ timeline
   })
   .to("#section2", {
     y: "-100%",
-    duration: 0.5, // Faster opacity transition
+    duration: 0.5,
     ease: "power2.inOut",
   });
 
 // Section 3 animation (typing effect)
-const typingText = document.querySelector("#typing-text");
-const textContent = typingText.textContent;
-typingText.textContent = "";
 timeline
   .to("#section3", {
     opacity: 1,
     duration: 0.25,
+    backgroundColor: "#000",
   })
   .call(
     () => {
@@ -91,60 +86,96 @@ timeline
     "+=0.5"
   )
   .to("#section3", {
+    opacity: 1,
+    duration: 1,
+  })
+  .to("#section3", {
     opacity: 0,
-    duration: 2.5,
-    delay: textContent.length * 0.05, // Add delay based on typing duration
+    duration: 1,
+    delay: textContent.length * 0.05,
   });
 
 // Section 4 animation (Who we are?)
-// timeline
-//   .to("#section4", {
-//     opacity: 1,
-//     duration: 1.5,
-//   })
-//   .to("#section4", {
-//     opacity: 0,
-//     duration: 1.5,
-//   });
-
-// Enable scrolling after preload
-// timeline.call(() => {
-//   document.body.style.overflow = "auto";
-//   gsap.utils.toArray(".section").forEach((section) => {
-//     section.style.display = "none";
-//   });
-// });
-
-// // Scroll animations for main sections
-gsap.utils.toArray(".main-section").forEach((section, index) => {
-  gsap.fromTo(
-    section,
+timeline
+  .fromTo(
+    "#section4",
     {
-      x: "100%",
-    },
-    {
-      x: "0%",
-      scrollTrigger: {
-        trigger: section,
-        start: "top top",
-        end: "bottom top",
-        scrub: true,
-      },
-    }
-  );
-
-  gsap.fromTo(
-    section,
-    {
-      opacity: 0,
-    },
-    {
+      y: "100%",
       opacity: 1,
-      scrollTrigger: {
-        trigger: section,
-        start: "top 80%",
-        toggleActions: "play none none reverse",
-      },
+      duration: 3,
+    },
+    {
+      y: "0%",
+      duration: 0.75,
+      ease: "power2.inOut",
     }
-  );
+  )
+  .to("#section4", {
+    opacity: 1,
+    duration: 0.75,
+    onStart: () => {
+      ["#section1", "#section2", "#section3"].forEach((selector) => {
+        document.querySelector(selector).style.display = "none";
+      });
+      const mainSection = document.querySelector(".main-section");
+      const questionMark = document.querySelector(".question-mark");
+      mainSection.style.zIndex = -1;
+      mainSection.style.opacity = 1;
+      questionMark.style.animation = "none";
+      questionMark.style.transform = "rotate(0deg)";
+      gsap.to(questionMark, {
+        rotation: [-15, 15, 0],
+        scale: [1.1, 0.9, 1],
+        duration: 0.25,
+        ease: "power2.inOut",
+        yoyo: true,
+        repeat: 1,
+      });
+    },
+  })
+  .to("#section4", {
+    y: "100%",
+    duration: 0.75,
+    ease: "power2.inOut",
+    onComplete: () => {
+      document.querySelector("#section4").style.display = "none";
+      document.querySelector(".main-section").style.zIndex = 0;
+      document.querySelector(".main-section").style.position = "relative";
+      document.querySelector("body").style.overflow = "auto";
+    },
+  });
+
+const seeMoreBtns = Array.from(document.querySelectorAll(".see-more"));
+seeMoreBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    // open sibling element which is dialog element
+    // disable scrolling
+    document.body.style.overflow = "hidden";
+    const dialog = btn.nextElementSibling;
+    dialog.showModal();
+    const closeBtn = dialog.querySelector(".close-btn");
+    closeBtn.addEventListener("click", () => {
+      dialog.close();
+      document.body.style.overflow = "auto";
+    });
+  });
+});
+
+const swiper = new Swiper(".swiper", {
+  direction: "vertical",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  mousewheel: true, // Enable mousewheel control
+  on: {
+    slideChange: function () {
+      const activeSlide = this.slides[this.activeIndex];
+      // position pagination inside the active slide
+      const pagination = document.querySelector(".swiper-pagination");
+      pagination.style.top = `${
+        activeSlide.offsetTop + activeSlide.offsetHeight / 2
+      }px`;
+    },
+  },
 });
